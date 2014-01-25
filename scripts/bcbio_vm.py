8d4a4fc5094cfb0e57278062b8418f42722793b1
chapmanb <chapmanb@50mail.com>
2014-01-25 13:15:06 -0500

    Re-work input script argument parsing as work towards supporting ipython runs

--- scripts/bcbio_vm.py
+++ scripts/bcbio_vm.py
@@ -27,6 +27,9 @@ def cmd_run(args):
     args = defaults.update_check_args(args, "Could not run analysis.")
     run.do_analysis(args, DOCKER)
 
+def cmd_ipython(args):
+    args = defaults.update_check_args(args, "Could not run IPython parallel analysis.")
+
 def cmd_server(args):
     args = defaults.update_check_args(args, "Could not run server.")
     ports = ["%s:%s" % (args.port, DOCKER["port"])]
@@ -37,13 +40,7 @@ def cmd_server(args):
 def cmd_save_defaults(args):
     defaults.save(args)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Automatic installation for bcbio-nextgen pipelines, with docker.")
-    parser.add_argument("--datadir", help="Directory to install genome data and associated files.",
-                        type=lambda x: (os.path.abspath(os.path.expanduser(x))))
-    subparsers = parser.add_subparsers(title="[sub-commands]")
-    # installation
+def _install_cmd(subparsers):
     parser_i = subparsers.add_parser("install", help="Install or upgrade bcbio-nextgen docker container and data.")
     parser_i.add_argument("--genomes", help="Genomes to download",
                           action="append", default=["GRCh37"],
@@ -60,24 +57,63 @@ if __name__ == "__main__":
     parser_i.add_argument("--wrapper", help="Update wrapper bcbio-nextgen-vm code",
                           action="store_true", default=False)
     parser_i.set_defaults(func=cmd_install)
-    # running
-    parser_r = subparsers.add_parser("run", help="Run an automated analysis.")
-    parser_r.add_argument("sample_config", help="YAML file with details about samples to process.")
-    parser_r.add_argument("--fcdir", help="A directory of Illumina output or fastq files to process",
-                          type=lambda x: (os.path.abspath(os.path.expanduser(x))))
-    parser_r.add_argument("--systemconfig", help="Global YAML configuration file specifying system details. "
-                          "Defaults to installed bcbio_system.yaml.")
-    parser_r.add_argument("-n", "--numcores", help="Total cores to use for processing",
-                          type=int, default=1)
+
+def _std_run_args(parser):
+    parser.add_argument("sample_config", help="YAML file with details about samples to process.")
+    parser.add_argument("--fcdir", help="A directory of Illumina output or fastq files to process",
+                        type=lambda x: (os.path.abspath(os.path.expanduser(x))))
+    parser.add_argument("--systemconfig", help="Global YAML configuration file specifying system details. "
+                        "Defaults to installed bcbio_system.yaml.")
+    parser.add_argument("-n", "--numcores", help="Total cores to use for processing",
+                        type=int, default=1)
+    return parser
+
+def _run_cmd(subparsers):
+    parser_r = subparsers.add_parser("run", help="Run an automated analysis on the local machine.")
+    parser_r = _std_run_args(parser_r)
     parser_r.set_defaults(func=cmd_run)
-    # server
+
+def _run_ipython_cmd(subparsers):
+    parser = subparsers.add_parser("ipython", help="Run on a cluster using IPython parallel.")
+    parser = _std_run_args(parser)
+    parser.add_argument("-s", "--scheduler", help="Scheduler to use.",
+                        choices=["lsf", "sge", "torque", "slurm"])
+    parser.add_argument("-q", "--queue", help="Scheduler queue to run jobs on.")
+    parser.add_argument("-r", "--resources",
+                        help=("Cluster specific resources specifications. Can be specified multiple times.\n"
+                              "Supports SGE and SLURM parameters."),
+                        default=[], action="append")
+    parser.add_argument("--timeout", help="Number of minutes before cluster startup times out. Defaults to 15",
+                        default=15, type=int)
+    parser.add_argument("--retries",
+                        help=("Number of retries of failed tasks during distributed processing. "
+                              "Default 0 (no retries)"),
+                        default=0, type=int)
+    parser.add_argument("-t", "--tag", help="Tag name to label jobs on the cluster",
+                        default="")
+    parser.set_defaults(func=cmd_ipython)
+
+def _server_cmd(subparsers):
     parser_s = subparsers.add_parser("server", help="Persistent REST server receiving requests via the specified port.")
     parser_s.add_argument("--port", default=8085, help="External port to connect to docker image.")
     parser_s.set_defaults(func=cmd_server)
-    # configuration
+
+def _config_cmd(subparsers):
     parser_c = subparsers.add_parser("saveconfig", help="Save standard configuration variables for current user. "
                                      "Avoids need to specify on the command line in future runs.")
     parser_c.set_defaults(func=cmd_save_defaults)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Automatic installation for bcbio-nextgen pipelines, with docker.")
+    parser.add_argument("--datadir", help="Directory with genome data and associated files.",
+                        type=lambda x: (os.path.abspath(os.path.expanduser(x))))
+    subparsers = parser.add_subparsers(title="[sub-commands]")
+    _install_cmd(subparsers)
+    _run_cmd(subparsers)
+    _run_ipython_cmd(subparsers)
+    _server_cmd(subparsers)
+    _config_cmd(subparsers)
     if len(sys.argv) == 1:
         parser.print_help()
     else:
