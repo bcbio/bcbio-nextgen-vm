8b2930ce6203f16d56ae60ab4fb5fbc34ad44036
chapmanb <chapmanb@50mail.com>
2014-02-18 05:47:25 -0500

    Generalize reconstitution of shared directories to temporary files. Labels files for remapping with nested context and adds ability to import associated files specified in input YAML and reference data files

--- bcbiovm/docker/remap.py
+++ bcbiovm/docker/remap.py
@@ -4,6 +4,7 @@ Handles identification of files from a YAML style file that need access, remappi
 docker and remapping results from docker.
 """
 from __future__ import print_function
+import os
 
 import six
 
@@ -35,7 +36,7 @@ def _mounts_to_out_dict(mounts):
         out[docker] = external
     return out
 
-def remap_fname(fname, remap_dict):
+def remap_fname(fname, context, remap_dict):
     """Remap a filename given potential remapping mount points.
     """
     matches = []
@@ -46,20 +47,28 @@ def remap_fname(fname, remap_dict):
     remap_orig, remap_new = matches[0]
     return fname.replace(remap_orig, remap_new)
 
-def walk_files(xs, f, remap_dict):
+def walk_files(xs, f, remap_dict, context=None):
     """Walk a set of input arguments, calling f on any files in the given remapping dictionary.
 
     xs is a JSON-like structure with lists, and dictionaries. This recursively
     calculates files nested inside these structures.
+
+    context keeps track of the nested set of keys associated with a file.
     """
     if isinstance(xs, (list, tuple)):
-        return [walk_files(x, f, remap_dict) for x in xs]
+        return [walk_files(x, f, remap_dict, context) for x in xs]
     elif isinstance(xs, dict):
         out = {}
+        if context is None:
+            context = []
         for k, v in xs.items():
-            out[k] = walk_files(v, f, remap_dict)
+            cur_context = context[:] + [k]
+            out[k] = walk_files(v, f, remap_dict, cur_context)
         return out
     elif xs and isinstance(xs, six.string_types) and xs.startswith(tuple(remap_dict.keys())):
-        return f(xs, remap_dict)
+        return f(xs, context, remap_dict)
+    elif (xs and isinstance(xs, six.string_types) and os.path.exists(xs) and os.path.isfile(xs)
+          and not remap_dict):
+        return f(xs, context, remap_dict)
     else:
         return xs
--- bcbiovm/ship/reconstitute.py
+++ bcbiovm/ship/reconstitute.py
@@ -8,18 +8,34 @@ import uuid
 import shutil
 
 from bcbio import utils
+from bcbio.log import logger
 from bcbiovm.docker import remap
 
 def prep_workdir(pack, args):
     """Unpack necessary files and directories into a temporary structure for processing
     """
     if pack["type"] == "shared":
-        workdir, new_args = _create_workdir_shared(pack["workdir"], args, pack["tmpdir"])
-        return workdir, new_args, _shared_finalizer(new_args, workdir, pack["workdir"])
+        workdir, remap_dict, new_args = _create_workdir_shared(pack["workdir"], args, pack["tmpdir"])
+        return workdir, new_args, _shared_finalizer(new_args, workdir, remap_dict)
     else:
         raise ValueError("Currently only handle shared filesystems")
 
 # ## Shared filesystem
+
+def _remap_dict_shared(workdir, new_workdir, args):
+    """Prepare a remap dictionary with directories we should potential copy files from.
+    """
+    ignore_keys = set(["algorithm"])
+    out = {workdir: new_workdir}
+    def _update_remap(fname, context, remap_dict):
+        """Updated list of directories we should potentially be remapping in.
+        """
+        if not fname.startswith(tuple(out.keys())) and context and context[0] not in ignore_keys:
+            dirname = os.path.normpath(os.path.dirname(fname))
+            local_dir = utils.safe_makedir(os.path.join(new_workdir, "external", str(len(out))))
+            out[dirname] = local_dir
+    remap.walk_files(args, _update_remap, {})
+    return out
 
 def _create_workdir_shared(workdir, args, tmpdir=None):
     """Create a work directory given inputs from the shared filesystem.
@@ -29,19 +45,21 @@ def _create_workdir_shared(workdir, args, tmpdir=None):
     argument paths at needed.
     """
     if not tmpdir:
-        return workdir, args
+        return workdir, {}, args
     else:
         new_workdir = utils.safe_makedir(os.path.join(tmpdir, "bcbio-work-%s" % uuid.uuid1()))
-        new_args = remap.walk_files(args, _remap_copy_file, {workdir: new_workdir})
-        return new_workdir, new_args
+        remap_dict = _remap_dict_shared(workdir, new_workdir, args)
+        new_args = remap.walk_files(args, _remap_copy_file, remap_dict)
+        return new_workdir, remap_dict, new_args
 
-def _remap_copy_file(fname, orig_to_temp):
+def _remap_copy_file(fname, context, orig_to_temp):
     """Remap file names and copy into temporary directory as needed.
 
     Handles simultaneous transfer of associated indexes.
     """
-    new_fname = remap.remap_fname(fname, orig_to_temp)
+    new_fname = remap.remap_fname(fname, context, orig_to_temp)
     if os.path.isfile(fname):
+        logger.info("** %s: %s" % (context, fname))
         utils.safe_makedir(os.path.dirname(new_fname))
         for ext in ["", ".idx", ".gbi", ".tbi", ".bai"]:
             if os.path.exists(fname + ext):
@@ -51,12 +69,14 @@ def _remap_copy_file(fname, orig_to_temp):
         utils.safe_makedir(new_fname)
     return new_fname
 
-def _shared_finalizer(args, workdir, orig_workdir):
+def _shared_finalizer(args, workdir, remap_dict):
     """Cleanup temporary working directory, copying missing files back to the shared workdir.
     """
     def _do(out):
-        if workdir != orig_workdir:
-            new_out = remap.walk_files(out, _remap_copy_file, {workdir: orig_workdir}) if out else None
+        if remap_dict:
+            new_remap_dict = {v: k for k, v in remap_dict.items()}
+            new_out = (remap.walk_files(out, _remap_copy_file, new_remap_dict)
+                       if out else None)
             if os.path.exists(workdir):
                 shutil.rmtree(workdir)
             return new_out
