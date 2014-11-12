c7fc4b18fa13e6fa4c6fbbec64f7988d60b7f7da
chapmanb <chapmanb@50mail.com>
2014-11-12 16:31:15 -0500

    Automate setting bcbio_system file to a target set of cores and memory. Include update as part of AWS bcbio bootstrapping

--- bcbiovm/aws/bootstrap.py
+++ bcbiovm/aws/bootstrap.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import sys
 
+import toolz as tz
+
 from bcbiovm.aws import icel
 
 def setup_cmd(awsparser):
@@ -22,11 +24,37 @@ def setup_cmd(awsparser):
                              "Ansible playbooks")
     parser.set_defaults(func=bootstrap)
 
-def bootstrap(args):
-    """Update bcbio_vm on worker nodes.
+# Core and memory usage for AWS instances
+AWS_INFO = {
+    "m3.large": [2, 3500],
+    "m3.xlarge": [4, 3500],
+    "m3.2xlarge": [8, 3500],
+    "c3.large": [2, 1750],
+    "c3.xlarge": [4, 1750],
+    "c3.2xlarge": [8, 1750],
+    "c3.4xlarge": [16, 1750],
+    "c3.8xlarge": [32, 1750],
+    "r3.large": [2, 7000],
+    "r3.xlarge": [4, 7000],
+    "r3.2xlarge": [8, 7000],
+    "r3.4xlarge": [16, 7000],
+    "r3.8xlarge": [32, 7000],
+}
 
-    Should eventually handle downloading genomes on demand as we support that.
+def bootstrap(args):
+    """Update bcbio_vm on worker nodes and set core and memory usage.
     """
     playbook_path = os.path.join(sys.prefix, "share", "bcbio-vm", "ansible",
                                  "roles", "bcbio_bootstrap", "tasks", "main.yml")
-    icel.run_ansible_pb(playbook_path, args)
+    def _calculate_cores_mem(args, cluster_config):
+        compute_nodes = int(tz.get_in(["nodes", "frontend", "compute_nodes"], cluster_config, 0))
+        if compute_nodes > 0:
+            machine = tz.get_in(["nodes", "compute", "flavor"], cluster_config)
+        else:
+            machine = tz.get_in(["nodes", "frontend", "flavor"], cluster_config)
+        cores, mem = AWS_INFO[machine]
+        # For small number of compute nodes, leave space for runner and controller
+        if compute_nodes < 5 and compute_nodes > 0:
+            cores = cores - 2
+        return {"target_cores": cores, "target_memory": mem}
+    icel.run_ansible_pb(playbook_path, args, _calculate_cores_mem)
--- bcbiovm/docker/devel.py
+++ bcbiovm/docker/devel.py
@@ -1,10 +1,17 @@
 """Utilities to help with developing using bcbio inside of docker.
 """
+import copy
+import datetime
+import math
 import os
+import shutil
 import subprocess
 
+import numpy
+import yaml
+
 from bcbio.provenance import do
-from bcbiovm.docker import install
+from bcbiovm.docker import defaults, install
 
 def setup_cmd(subparsers):
     parser = subparsers.add_parser("devel", help="Utilities to help with develping using bcbion inside of docker")
@@ -14,6 +21,11 @@ def setup_cmd(subparsers):
     iparser.add_argument("-i", "--image", help="Image name to write updates to",
                          default=install.DEFAULT_IMAGE)
     iparser.set_defaults(func=_run_setup_install)
+
+    sparser = psub.add_parser("system", help="Update bcbio system file with a given core and memory/core target")
+    sparser.add_argument("cores", help="Target cores to use for multi-core processes")
+    sparser.add_argument("memory", help="Target memory per core, in Mb (1000 = 1Gb)")
+    sparser.set_defaults(func=_run_system_update)
 
 def _run_setup_install(args):
     """Install python code from a bcbio-nextgen development tree inside of docker.
@@ -31,3 +43,76 @@ def _run_setup_install(args):
     subprocess.check_call(["docker", "commit", cid, args.image])
     subprocess.check_call(["docker", "rm", cid], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print("Updated bcbio-nextgen install in docker container: %s" % args.image)
+
+def _run_system_update(args):
+    """Update bcbio_system.yaml file with a given target of cores and memory.
+    """
+    mem_types = set(["memory", "jvm_opts"])
+    args = defaults.update_check_args(args, "Could not do upgrade of bcbio_system.yaml")
+    system_file = os.path.join(args.datadir, "galaxy", "bcbio_system.yaml")
+    with open(system_file) as in_handle:
+        config = yaml.safe_load(in_handle)
+    out = copy.deepcopy(config)
+    mems = []
+    for attrs in config.get("resources", {}).itervalues():
+        for key, value in attrs.iteritems():
+            if key in mem_types:
+                mems.append((key, value))
+    common_mem = _calculate_common_memory(mems)
+    for prog, attrs in config.get("resources", {}).iteritems():
+        for key, value in attrs.iteritems():
+            if key == "cores":
+                out['resources'][prog][key] = int(args.cores)
+            elif key in mem_types:
+                out["resources"][prog][key] = _update_memory(key, value, args.memory,
+                                                             common_mem)
+    bak_file = system_file + ".bak%s" % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    shutil.move(system_file, bak_file)
+    with open(system_file, "w") as out_handle:
+        yaml.safe_dump(out, out_handle, default_flow_style=False, allow_unicode=False)
+
+def _get_cur_mem(key, val):
+    if key == "memory":
+        cur_mem = val
+    elif key == "jvm_opts":
+        cur_mem = val[1].replace("-Xmx", "")
+    cur_val = int(cur_mem[:-1])
+    cur_mod = cur_mem[-1:]
+    if cur_mod.lower() == "g":
+        cur_val = cur_val * 1000
+    else:
+        assert cur_mod.lower() == "m"
+    return cur_val, cur_mod
+
+def _calculate_common_memory(kvs):
+    """Get the median memory specification, in megabytes.
+    """
+    mems = []
+    for key, val in kvs:
+        cur_val, _ = _get_cur_mem(key, val)
+        mems.append(cur_val)
+    return numpy.median(mems)
+
+def _update_memory(key, cur, target, common_mem):
+    """Update memory specifications to match target.
+
+    Handles JVM options and both megabyte and gigabyte specifications.
+    `target` is in megabytes. Does not adjust down memory that is more
+    than 1.5x the current common memory setting, assuming these are pre-set for
+    higher memory requirements.
+    """
+    cur_mem, orig_mod = _get_cur_mem(key, cur)
+    if cur_mem >= common_mem * 1.5:
+        return cur
+    else:
+        if orig_mod.lower() == "g":
+            target = int(math.floor(float(target) / 1000.0))
+        else:
+            target = int(target)
+        new_val = "%s%s" % (target, orig_mod)
+        if key == "jvm_opts":
+            out = cur
+            out[-1] = "-Xmx%s" % new_val
+        else:
+            out = new_val
+        return out
