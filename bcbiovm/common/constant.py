d9620a23876542bbb85cb8aa3e6e8708f5710864
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-05-02 16:09:28 +0300

    Add wrapper over the elasticluster functionalities

--- bcbiovm/common/constant.py
+++ bcbiovm/common/constant.py
@@ -1,6 +1,17 @@
 """
 Shared constants across the bcbio-nextgen-vm project.
 """
+import os
+
+NFS_OPTIONS = "rw,async,nfsvers=3"  # NFS tuning
+
+
+class PATH:
+    BCBIO = os.path.join(os.path.expanduser("~"), '.bcbio')
+    EC = os.path.join(BCBIO, "elasticluster")
+    EC_CONFIG = os.path.join(EC, "config")
+    EC_STORAGE = os.path.join(EC, "storage")
+    PICKLE_FILE = os.path.join(EC_STORAGE, "%(cluster)s.pickle")
 
 
 class LOG:
