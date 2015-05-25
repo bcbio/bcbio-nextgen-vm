941174ac557a115da426fe6f02e90337c0b27715
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-05-25 12:40:46 +0300

    Refactor bcbiovm.docker.devel command line parser

--- bcbiovm/client/commands.py
+++ bcbiovm/client/commands.py
@@ -16,6 +16,7 @@ from bcbio.graph import graph
 from bcbiovm.client import base
 from bcbiovm.common import constant
 from bcbiovm.docker import defaults as docker_defaults
+from bcbiovm.docker import devel as docker_devel
 from bcbiovm.docker import install as docker_install
 from bcbiovm.provider import factory as cloud_factory
 from bcbiovm.provider.aws.clusterk import main as clusterk_main
@@ -336,6 +337,147 @@ class ClusterSSH(base.BaseCommand):
                             verbose=self.args.verbose)
 
 
+class DockerBuild(base.BaseCommand):
+
+    """Build docker image and export to S3."""
+
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        parser = self._main_parser.add_parser(
+            "dockerbuild",
+            help="Build docker image and export to S3")
+        parser.add_argument(
+            "-b", "--bucket", default="bcbio_nextgen",
+            help="S3 bucket to upload the gzipped docker image to")
+        parser.add_argument(
+            "-t", "--buildtype", default="full", choices=["full", "code"],
+            help=("Type of docker build to do. full is all code and third"
+                  " party tools. code is only bcbio-nextgen code."))
+        parser.add_argument(
+            "-d", "--rundir", default="/tmp/bcbio-docker-build",
+            help="Directory to run docker build in")
+        parser.add_argument(
+            "-q", "--quiet", dest="verbose", action="store_false",
+            default=True, help="Quiet output when running Ansible playbooks")
+        parser.set_defaults(func=self.run)
+
+    def process(self):
+        """Run the command with the received information."""
+        return docker_devel.run_docker_build(self.args)
+
+
+class DockerBiodataUpload(base.BaseCommand):
+
+    """Upload pre-prepared biological data to cache."""
+
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        parser = self._main_parser.add_parser(
+            "biodata",
+            help="Upload pre-prepared biological data to cache")
+        parser.add_argument(
+            "--prepped",
+            help=("Start with an existing set of cached data to "
+                  "output directory."))
+        parser.add_argument(
+            "--genomes", help="Genomes to download",
+            action="append", default=[],
+            choices=["GRCh37", "hg19", "mm10", "mm9", "rn5", "canFam3", "dm3",
+                     "Zv9", "phix", "sacCer3", "xenTro3", "TAIR10",
+                     "WBcel235"])
+        parser.add_argument(
+            "--aligners", help="Aligner indexes to download",
+            action="append", default=[],
+            choices=["bowtie", "bowtie2", "bwa", "novoalign", "star", "ucsc"])
+        parser.add_argument(
+            "-q", "--quiet", dest="verbose", action="store_false",
+            default=True, help="Quiet output when running Ansible playbooks")
+        parser.set_defaults(func=self.run)
+
+    def process(self):
+        """Manage preparation of biodata on a local machine, uploading
+        to S3 in pieces."""
+        return docker_devel.run_biodata_upload(self.args)
+
+
+class DockerSystemUpdate(base.BaseCommand):
+
+    """Update bcbio system file with a given core and memory/core target."""
+
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        parser = self._main_parser.add_parser(
+            "system",
+            help=("Update bcbio system file with a given core "
+                  "and memory/core target"))
+        parser.add_argument(
+            "cores",
+            help="Target cores to use for multi-core processes")
+        parser.add_argument(
+            "memory",
+            help="Target memory per core, in Mb (1000 = 1Gb)")
+        parser.add_argument(
+            "-q", "--quiet", dest="verbose", action="store_false",
+            default=True, help="Quiet output when running Ansible playbooks")
+        parser.set_defaults(func=self.run)
+
+    def process(self):
+        """Update bcbio_system.yaml file with a given target of cores
+        and memory.
+        """
+        return docker_devel.run_system_update(self.args)
+
+
+class DockerSetupInstall(base.BaseCommand):
+
+    """Run a python setup.py install inside of the current directory."""
+
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        parser = self._main_parser.add_parser(
+            "setup_install",
+            help=("Run a python setup.py install inside of "
+                  "the current directory"))
+        parser.add_argument(
+            "-i", "--image", help="Image name to write updates to",
+            default=constant.DOCKER_DEFAULT_IMAGE)
+        parser.add_argument(
+            "-q", "--quiet", dest="verbose", action="store_false",
+            default=True, help="Quiet output when running Ansible playbooks")
+        parser.set_defaults(func=self.run)
+
+    def process(self):
+        """Install python code from a bcbio-nextgen development tree
+        inside of docker.
+        """
+        return docker_devel.run_setup_install(self.args)
+
+
+class DockerDevel(base.BaseCommand):
+
+    """Utilities to help with develping using bcbion inside of docker."""
+
+    sub_commands = [
+        (DockerBuild, "actions"),
+        (DockerBiodataUpload, "actions"),
+        (DockerSetupInstall, "actions"),
+        (DockerSystemUpdate, "actions")
+    ]
+
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        parser = self._main_parser.add_parser(
+            "devel",
+            help=("Utilities to help with develping using bcbion"
+                  "inside of docker."))
+        actions = parser.add_subparsers(title="[devel commands]")
+        self._register_parser(actions, "actions")
+
+    def process(self):
+        """Run the command with the received information."""
+        pass
+
+
 class ElastiCluster(base.BaseCommand):
 
     """Run and manage a cluster using elasticluster."""
@@ -360,7 +502,7 @@ class ElastiCluster(base.BaseCommand):
         pass
 
 
-class ClusterK(object):
+class ClusterK(base.BaseCommand):
 
     def setup(self):
         """Extend the parser configuration in order to expose this command."""
--- bcbiovm/docker/devel.py
+++ bcbiovm/docker/devel.py
@@ -7,7 +7,6 @@ import os
 import shutil
 import subprocess
 
-import boto
 import numpy
 import yaml
 
@@ -19,130 +18,6 @@ from bcbio.provenance import do
 from bcbiovm.common import cluster as clusterops
 from bcbiovm.common import constant
 from bcbiovm.docker import defaults, install, manage, mounts
-
-
-def add_biodata_args(parser):
-    """Add standard arguments for preparing biological data to a
-    command line arg parser.
-    """
-    parser.add_argument(
-        "--genomes", help="Genomes to download",
-        action="append", default=[],
-        choices=["GRCh37", "hg19", "mm10", "mm9", "rn5", "canFam3", "dm3",
-                 "Zv9", "phix", "sacCer3", "xenTro3", "TAIR10", "WBcel235"])
-    parser.add_argument(
-        "--aligners", help="Aligner indexes to download",
-        action="append", default=[],
-        choices=["bowtie", "bowtie2", "bwa", "novoalign", "star", "ucsc"])
-    return parser
-
-
-def setup_cmd(subparsers):
-    parser = subparsers.add_parser(
-        "devel",
-        help="Utilities to help with develping using bcbion inside of docker")
-    psub = parser.add_subparsers(title="[devel commands]")
-
-    sparser = psub.add_parser(
-        "system",
-        help=("Update bcbio system file with a given core "
-              "and memory/core target"))
-    sparser.add_argument(
-        "cores",
-        help="Target cores to use for multi-core processes")
-    sparser.add_argument(
-        "memory",
-        help="Target memory per core, in Mb (1000 = 1Gb)")
-    sparser.set_defaults(func=_run_system_update)
-
-    dparser = psub.add_parser(
-        "biodata",
-        help="Upload pre-prepared biological data to cache")
-    dparser.add_argument(
-        "--prepped",
-        help="Start with an existing set of cached data to output directory.")
-    dparser = add_biodata_args(dparser)
-    dparser.set_defaults(func=_run_biodata_upload)
-
-    dbparser = psub.add_parser(
-        "dockerbuild",
-        help="Build docker image and export to S3")
-    dbparser.add_argument(
-        "-b", "--bucket", default="bcbio_nextgen",
-        help="S3 bucket to upload the gzipped docker image to")
-    dbparser.add_argument(
-        "-t", "--buildtype", default="full", choices=["full", "code"],
-        help=("Type of docker build to do. full is all code and third party"
-              " tools. code is only bcbio-nextgen code."))
-    dbparser.add_argument(
-        "-d", "--rundir", default="/tmp/bcbio-docker-build",
-        help="Directory to run docker build in")
-    parser.add_argument(
-        "-q", "--quiet", dest="verbose", action="store_false",
-        default=True, help="Quiet output when running Ansible playbooks")
-    dbparser.set_defaults(func=_run_docker_build)
-
-
-def run_setup_install(args):
-    """Install python code from a bcbio-nextgen development tree
-    inside of docker.
-    """
-    # Install code to docker image
-    bmounts = ["-v", "%s:%s" % (os.getcwd(), "/tmp/bcbio-nextgen")]
-    bash_cmd = ("rm -rf /usr/local/share/bcbio-nextgen/anaconda/lib/"
-                "python2.7/site-packages/bcbio && cd /tmp/bcbio-nextgen && "
-                "/usr/local/share/bcbio-nextgen/anaconda/bin/python "
-                "setup.py install")
-    cmd = ["docker", "run", "-i", "-d", "--net=host"]
-    cmd.extend(bmounts)
-    cmd.extend([args.image, "bash", "-l", "-c", bash_cmd])
-    # TODO(alexandrucoman): Use bcbiovm.common.utils.execute
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    cid = process.communicate()[0].strip()
-    do.run(["docker", "attach", "--no-stdin", cid],
-           "Running in docker container: %s" % cid,
-           log_stdout=True)
-
-    # TODO(alexandrucoman): Use bcbiovm.common.utils.execute
-    subprocess.check_call(["docker", "commit", cid, args.image])
-    subprocess.check_call(["docker", "rm", cid], stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
-    print("Updated bcbio-nextgen install in docker container: %s" % args.image)
-
-
-def _run_system_update(args):
-    """Update bcbio_system.yaml file with a given target of cores
-    and memory.
-    """
-    # Update bcbio_system.yaml
-    mem_types = set(["memory", "jvm_opts"])
-    args = defaults.update_check_args(
-        args, "Could not do upgrade of bcbio_system.yaml")
-    system_file = os.path.join(args.datadir, "galaxy", "bcbio_system.yaml")
-    with open(system_file) as in_handle:
-        config = yaml.safe_load(in_handle)
-    out = copy.deepcopy(config)
-    mems = []
-    for attrs in config.get("resources", {}).itervalues():
-        for key, value in attrs.iteritems():
-            if key in mem_types:
-                mems.append((key, value))
-    common_mem = _calculate_common_memory(mems)
-    for prog, attrs in config.get("resources", {}).iteritems():
-        for key, value in attrs.iteritems():
-            if key == "cores":
-                out['resources'][prog][key] = int(args.cores)
-            elif key in mem_types:
-                out["resources"][prog][key] = _update_memory(key, value,
-                                                             args.memory,
-                                                             common_mem)
-    # TODO(alexandrucoman): Add an utility for creating backup files
-    now = datetime.datetime.now()
-    bak_file = system_file + ".bak%s" % now.strftime("%Y-%m-%d-%H-%M-%S")
-    shutil.move(system_file, bak_file)
-    with open(system_file, "w") as out_handle:
-        yaml.safe_dump(out, out_handle, default_flow_style=False,
-                       allow_unicode=False)
 
 
 def _get_cur_mem(key, val):
@@ -191,63 +66,6 @@ def _update_memory(key, cur, target, common_mem):
         return out
 
 
-def _run_docker_build(args):
-    # Build docker images
-    inventory_path = os.path.join(constant.PATH.ANSIBLE_BASE,
-                                  "standard_hosts.txt")
-
-    def extra_vars(cluster_config):
-        """Extra variables to inject into a playbook."""
-        return {
-            "bcbio_bucket": args.bucket,
-            "docker_buildtype": args.buildtype,
-            "bcbio_dir": args.rundir
-        }
-
-    playbook = clusterops.AnsiblePlaybook(
-        inventory_path=inventory_path,
-        playbook_path=constant.PLAYBOOK.DOCKER_LOCAl,
-        config=args.econfig,
-        cluster=args.cluster,
-        verbose=args.verbose,
-        extra_vars=extra_vars)
-    return playbook.run()
-
-
-def _run_biodata_upload(args):
-    """Manage preparation of biodata on a local machine, uploading
-    to S3 in pieces.
-    """
-    # ## Upload pre-build biological data
-    args = defaults.update_check_args(args, "biodata not uploaded")
-    args = install.docker_image_arg(args)
-    for gbuild in args.genomes:
-        print("Preparing %s" % gbuild)
-        if args.prepped:
-            for target in ["samtools"] + args.aligners:
-                genome.download_prepped_genome(gbuild, {}, target, False,
-                                               args.prepped)
-            print("Downloaded prepped %s to %s. Edit and re-run without "
-                  "--prepped to upload" % (gbuild, args.prepped))
-            return
-        cmdline = ["upgrade", "--genomes", gbuild]
-        for aligner in args.aligners:
-            cmdline += ["--aligners", aligner]
-        dmounts = mounts.prepare_system(args.datadir,
-                                        constant.DOCKER['biodata_dir'])
-        manage.run_bcbio_cmd(args.image, dmounts, cmdline)
-        print("Uploading %s" % gbuild)
-        gdir = _get_basedir(args.datadir, gbuild)
-        basedir, genomedir = os.path.split(gdir)
-        assert genomedir == gbuild
-        with utils.chdir(basedir):
-            all_dirs = sorted(os.listdir(gbuild))
-            _upload_biodata(gbuild, "seq", all_dirs)
-            for aligner in args.aligners:
-                _upload_biodata(
-                    gbuild, genome.REMAP_NAMES.get(aligner, aligner), all_dirs)
-
-
 def _upload_biodata(gbuild, target, all_dirs):
     """Upload biodata for a specific genome build and target to S3.
     """
@@ -282,3 +100,122 @@ def _get_basedir(datadir, target_genome):
     for dirname in glob.glob(os.path.join(genome_dir, "*", "*")):
         if dirname.endswith("/%s" % target_genome):
             return dirname
+
+
+def run_setup_install(args):
+    """Install python code from a bcbio-nextgen development tree
+    inside of docker.
+    """
+    # Install code to docker image
+    bmounts = ["-v", "%s:%s" % (os.getcwd(), "/tmp/bcbio-nextgen")]
+    bash_cmd = ("rm -rf /usr/local/share/bcbio-nextgen/anaconda/lib/"
+                "python2.7/site-packages/bcbio && cd /tmp/bcbio-nextgen && "
+                "/usr/local/share/bcbio-nextgen/anaconda/bin/python "
+                "setup.py install")
+    cmd = ["docker", "run", "-i", "-d", "--net=host"]
+    cmd.extend(bmounts)
+    cmd.extend([args.image, "bash", "-l", "-c", bash_cmd])
+    # TODO(alexandrucoman): Use bcbiovm.common.utils.execute
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    cid = process.communicate()[0].strip()
+    do.run(["docker", "attach", "--no-stdin", cid],
+           "Running in docker container: %s" % cid,
+           log_stdout=True)
+
+    # TODO(alexandrucoman): Use bcbiovm.common.utils.execute
+    subprocess.check_call(["docker", "commit", cid, args.image])
+    subprocess.check_call(["docker", "rm", cid], stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT)
+    print("Updated bcbio-nextgen install in docker container: %s" % args.image)
+
+
+def run_system_update(args):
+    """Update bcbio_system.yaml file with a given target of cores
+    and memory.
+    """
+    # Update bcbio_system.yaml
+    mem_types = set(["memory", "jvm_opts"])
+    args = defaults.update_check_args(
+        args, "Could not do upgrade of bcbio_system.yaml")
+    system_file = os.path.join(args.datadir, "galaxy", "bcbio_system.yaml")
+    with open(system_file) as in_handle:
+        config = yaml.safe_load(in_handle)
+    out = copy.deepcopy(config)
+    mems = []
+    for attrs in config.get("resources", {}).itervalues():
+        for key, value in attrs.iteritems():
+            if key in mem_types:
+                mems.append((key, value))
+    common_mem = _calculate_common_memory(mems)
+    for prog, attrs in config.get("resources", {}).iteritems():
+        for key, value in attrs.iteritems():
+            if key == "cores":
+                out['resources'][prog][key] = int(args.cores)
+            elif key in mem_types:
+                out["resources"][prog][key] = _update_memory(key, value,
+                                                             args.memory,
+                                                             common_mem)
+    # TODO(alexandrucoman): Add an utility for creating backup files
+    now = datetime.datetime.now()
+    bak_file = system_file + ".bak%s" % now.strftime("%Y-%m-%d-%H-%M-%S")
+    shutil.move(system_file, bak_file)
+    with open(system_file, "w") as out_handle:
+        yaml.safe_dump(out, out_handle, default_flow_style=False,
+                       allow_unicode=False)
+
+
+def run_docker_build(args):
+    # Build docker images
+    inventory_path = os.path.join(constant.PATH.ANSIBLE_BASE,
+                                  "standard_hosts.txt")
+
+    def extra_vars(cluster_config):
+        """Extra variables to inject into a playbook."""
+        return {
+            "bcbio_bucket": args.bucket,
+            "docker_buildtype": args.buildtype,
+            "bcbio_dir": args.rundir
+        }
+
+    playbook = clusterops.AnsiblePlaybook(
+        inventory_path=inventory_path,
+        playbook_path=constant.PLAYBOOK.DOCKER_LOCAl,
+        config=args.econfig,
+        cluster=args.cluster,
+        verbose=args.verbose,
+        extra_vars=extra_vars)
+    return playbook.run()
+
+
+def run_biodata_upload(args):
+    """Manage preparation of biodata on a local machine, uploading
+    to S3 in pieces.
+    """
+    # ## Upload pre-build biological data
+    args = defaults.update_check_args(args, "biodata not uploaded")
+    args = install.docker_image_arg(args)
+    for gbuild in args.genomes:
+        print("Preparing %s" % gbuild)
+        if args.prepped:
+            for target in ["samtools"] + args.aligners:
+                genome.download_prepped_genome(gbuild, {}, target, False,
+                                               args.prepped)
+            print("Downloaded prepped %s to %s. Edit and re-run without "
+                  "--prepped to upload" % (gbuild, args.prepped))
+            return
+        cmdline = ["upgrade", "--genomes", gbuild]
+        for aligner in args.aligners:
+            cmdline += ["--aligners", aligner]
+        dmounts = mounts.prepare_system(args.datadir,
+                                        constant.DOCKER['biodata_dir'])
+        manage.run_bcbio_cmd(args.image, dmounts, cmdline)
+        print("Uploading %s" % gbuild)
+        gdir = _get_basedir(args.datadir, gbuild)
+        basedir, genomedir = os.path.split(gdir)
+        assert genomedir == gbuild
+        with utils.chdir(basedir):
+            all_dirs = sorted(os.listdir(gbuild))
+            _upload_biodata(gbuild, "seq", all_dirs)
+            for aligner in args.aligners:
+                _upload_biodata(
+                    gbuild, genome.REMAP_NAMES.get(aligner, aligner), all_dirs)
