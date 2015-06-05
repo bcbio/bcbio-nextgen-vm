e92e669960eee8550a53e43c73f48933d8ca1dc9
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-06-05 13:05:38 +0300

    Add Azure instances types

--- bcbiovm/provider/azure/azure_provider.py
+++ bcbiovm/provider/azure/azure_provider.py
@@ -1,5 +1,6 @@
 """Azure Cloud Provider for bcbiovm."""
 
+from bcbiovm.common import objects
 from bcbiovm.provider import base
 
 # pylint: disable=no-self-use
@@ -16,7 +17,58 @@ class AzureProvider(base.BaseCloudProvider):
         """Returns a dictionary with all the flavors available for the current
         cloud provider.
         """
-        return {}
+        # More information regarding Azure instances types can be found on the
+        # following link: https://goo.gl/mEjiC5
+
+        return {
+            "ExtraSmall": objects.Flavor(cpus=1, memory=768),
+            "Small": objects.Flavor(cpus=1, memory=1792),
+            "Medium": objects.Flavor(cpus=2, memory=3584),
+            "Large": objects.Flavor(cpus=4, memory=7168),
+            "ExtraLarge": objects.Flavor(cpus=8, memory=14336),
+
+            # General Purpose: For websites, small-to-medium databases,
+            #                  and other everyday applications.
+            "A0": objects.Flavor(cpus=1, memory=768),       # 0.75 GB
+            "A1": objects.Flavor(cpus=1, memory=1792),      # 1.75 GB
+            "A2": objects.Flavor(cpus=2, memory=3584),      # 3.50 GB
+            "A3": objects.Flavor(cpus=4, memory=7168),      # 7.00 GB
+            "A4": objects.Flavor(cpus=8, memory=14336),     # 14.00 GB
+
+            # Memory Intensive: For large databases, SharePoint server farms,
+            #                   and high-throughput applications.
+            "A5": objects.Flavor(cpus=2, memory=14336),     # 14.00 GB
+            "A6": objects.Flavor(cpus=4, memory=28672),     # 28.00 GB
+            "A7": objects.Flavor(cpus=8, memory=57344),     # 56.00 GB
+
+            # Network optimized: Ideal for Message Passing Interface (MPI)
+            #                    applications, high-performance clusters,
+            #                    modeling and simulations and other compute
+            #                    or network intensive scenarios.
+            "A8": objects.Flavor(cpus=8, memory=57344),     # 56.00 GB
+            "A9": objects.Flavor(cpus=16, memory=114688),   # 112.00 GB
+
+            # Compute Intensive: For high-performance clusters, modeling
+            #                    and simulations, video encoding, and other
+            #                    compute or network intensive scenarios.
+            "A10": objects.Flavor(cpus=8, memory=57344),     # 56.00 GB
+            "A11": objects.Flavor(cpus=16, memory=114688),   # 112.00 GB
+
+            # Optimized compute: 60% faster CPUs, more memory, and local SSD
+            # General Purpose: For websites, small-to-medium databases,
+            #                  and other everyday applications.
+            "D1": objects.Flavor(cpus=1, memory=3584),      # 3.50 GB
+            "D2": objects.Flavor(cpus=2, memory=7168),      # 7.00 GB
+            "D3": objects.Flavor(cpus=4, memory=14336),     # 14.00 GB
+            "D4": objects.Flavor(cpus=8, memory=28672),     # 28.00 GB
+
+            # Memory Intensive: For large databases, SharePoint server farms,
+            #                   and high-throughput applications.
+            "D11": objects.Flavor(cpus=2, memory=14336),     # 14.00 GB
+            "D12": objects.Flavor(cpus=4, memory=28672),     # 28.00 GB
+            "D13": objects.Flavor(cpus=8, memory=57344),     # 56.00 GB
+            "D14": objects.Flavor(cpus=16, memory=114688),   # 112.00 GB
+        }
 
     def information(self, config, cluster, verbose=False):
         """
