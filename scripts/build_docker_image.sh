3c7027d1b00f97edb8b09354aa2ddd1759f5163c
chapmanb <chapmanb@50mail.com>
2015-08-01 16:33:21 -0400

    Update for latest Docker with devicemapper
    - Update AWS instance in config to point to latest Ubuntu 14.04.02
    - Point to current docker distribution and apt resources
    - No longer use btrfs workaround for aufs, which does not work
      on latest AWS instances without attached device storage.
    - When building docker image, run on larger /encrypted storage to
      avoid filling up disk.

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-set -e
+set -eu -o pipefail
+# setup for local ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 ssh-keyscan -t rsa localhost >> ~/.ssh/known_hosts
+# change docker mount location to larger /encrypted partition
+sudo service docker stop
+sudo mv /var/lib/docker /encrypted/docker
+sudo mkdir -p /encrypted/docker-tmp
+sudo sh -c "echo 'DOCKER_OPTS=\"-g /encrypted/docker\"' >> /etc/default/docker"
+sudo sh -c "echo 'export TMPDIR=\"/encrypted/docker-tmp\"' >> /etc/default/docker"
+sudo service docker start
+# Run docker build
 bcbio_vm.py devel dockerbuild
