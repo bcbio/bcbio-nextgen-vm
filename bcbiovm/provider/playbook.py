586d8f97769d3ef007710bb9083dd5f9c0007a34
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-08-18 13:32:36 +0300

    Update Docker playbooks

--- bcbiovm/provider/playbook.py
+++ bcbiovm/provider/playbook.py
@@ -9,6 +9,7 @@ class Playbook(object):
 
     base = (sys.prefix, "share", "bcbio-vm", "ansible")
     bcbio = ("roles", "bcbio_bootstrap", "tasks", "main.yml")
+    docker = ("roles", "docker", "tasks", "main.yml")
     docker_local = ("bcbio_vm_docker_local.yml", )
     nfs = ("roles", "encrypted_nfs", "tasks", "main.yml")
 
@@ -29,7 +30,6 @@ class AWSPlaybook(Playbook):
 
     """Default paths for Ansible playbooks."""
 
-    docker = ("roles", "aws_docker", "tasks", "main.yml")
     gof3r = ("roles", "gof3r", "tasks", "main.yml")
     icel = ("roles", "icel", "tasks", "main.yml")
     mount_lustre = ("roles", "lustre_client", "tasks", "mount.yml")
@@ -39,5 +39,3 @@ class AWSPlaybook(Playbook):
 class AzurePlaybook(Playbook):
 
     """Default paths for Ansible playbooks."""
-
-    docker = ("roles", "azure_docker", "tasks", "main.yml")
