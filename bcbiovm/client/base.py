db39fc91de1545a89966131aca0374bde1f23d4e
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-09-14 13:22:48 +0300

    Add base subcommand for Docker

--- bcbiovm/client/base.py
+++ bcbiovm/client/base.py
@@ -6,13 +6,17 @@ Client base-classes:
 
 import abc
 import collections
+import os
+import sys
 
 import six
+import yaml
 
 from bcbiovm.common import exception
+from bcbiovm.common import objectstore
 from bcbiovm.common import utils
 
-__all__ = ['BaseCommand', 'BaseParser']
+__all__ = ['BaseCommand', 'BaseParser', "DockerSubcommand"]
 
 LOG = utils.get_logger(__name__)
 SubCommand = collections.namedtuple("SubCommand", ["name", "instance"])
@@ -267,3 +271,124 @@ class BaseParser(object):
 
         # pylint: disable=no-member
         return self._args.func()
+
+
+class DockerSubcommand(BaseCommand):
+
+    """Subcommand that contains utilities for docker."""
+
+    _DATADIR = os.path.realpath(os.path.normpath(
+        os.path.join(os.path.dirname(sys.executable), os.pardir, os.pardir,
+                     "data")))
+    _INSTALL_PARAMS = os.path.join(_DATADIR, "config", "install-params.yaml")
+    _BCBIO_SYSTEM = os.path.join(_DATADIR, "galaxy", "bcbio_system.yaml")
+
+    def __init__(self, *args, **kwargs):
+        super(DockerSubcommand, self).__init__(*args, **kwargs)
+        self._defaults = {
+            "datadir": None,
+        }
+        self._need_datadir = True
+        self._need_prologue = False
+
+    def _get_config_file(self, just_filename=False):
+        """Retrieve standard user configuration file.
+
+        Uses location from appdirs (https://github.com/ActiveState/appdirs).
+        Could pull this in as dependency for more broad platform support.
+        """
+        config_dir = os.path.join(os.getenv('XDG_CONFIG_HOME',
+                                            os.path.expanduser("~/.config")),
+                                  "bcbio-nextgen")
+        config_file = os.path.join(config_dir, "bcbio-docker-config.yaml")
+
+        if not os.path.exists(config_dir):
+            os.makedirs(config_dir)
+
+        if just_filename or os.path.exists(config_file):
+            return config_file
+
+    def _get_defaults(self):
+        """Retrieve saved default configurations."""
+        defaults = {}
+        config_file = self._get_config_file()
+
+        if config_file:
+            with open(config_file) as in_handle:
+                try:
+                    defaults = yaml.load(in_handle)
+                except ValueError as exc:
+                    LOG.exception("Failed to load user configuration "
+                                  "file: %(reason)s", {"reason": exc})
+
+        return defaults
+
+    def _get_datadir(self):
+        """Check if the default data directory/standard setup is present."""
+        if not self._need_datadir:
+            return self._DATADIR
+
+        for path in (self._INSTALL_PARAMS, self._BCBIO_SYSTEM):
+            if not os.path.exists(path):
+                return None
+
+        return self._DATADIR
+
+    def _add_defaults(self):
+        """Add user configured defaults to supplied command line arguments."""
+        config_defaults = self._get_defaults()
+        for config, value in self._defaults.items():
+            args_value = getattr(self.args, config, None)
+            if not args_value or args_value == value:
+                if config in config_defaults:
+                    setattr(self.args, config, config_defaults[config])
+
+    def _handle_remotes(self):
+        """Retrieve supported remote inputs specified on the command line."""
+        sample_config = getattr(self.args, "sample_config", None)
+        if not sample_config:
+            LOG.debug("The sample_config was not provided.")
+            return
+
+        if os.path.isfile(sample_config):
+            LOG.debug("The sample config file is on the local storage.")
+            return
+
+        for manager in (objectstore.AmazonS3, objectstore.AzureBlob):
+            if not manager.check_resource(sample_config):
+                continue
+
+            LOG.debug("The sample config is remote, using: %(name)s",
+                      {"name": manager.__name__})
+            self.args.sample_config = manager.load_config(sample_config)
+            return
+
+        raise NotImplementedError("Do not recognize remote input %(sample)s" %
+                                  {"sample": self.args.sample_config})
+
+    def prologue(self):
+        """Executed once before the arguments parsing."""
+        if not self._need_prologue:
+            return
+
+        self._add_defaults()
+        self._handle_remotes()
+
+        if not self.args.datadir:
+            default_datadir = self._get_datadir()
+            if default_datadir:
+                self.args.datadir = default_datadir
+            else:
+                LOG.error("Must specify a `--datadir` or save the default "
+                          "location with `saveconfig`.")
+                sys.exit(1)
+
+    @abc.abstractmethod
+    def setup(self):
+        """Extend the parser configuration in order to expose this command."""
+        pass
+
+    @abc.abstractmethod
+    def process(self):
+        """Override this with your desired procedures."""
+        pass
