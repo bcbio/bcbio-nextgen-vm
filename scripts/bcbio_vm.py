06c9cf6484f5cac819fa4026a51605a0a1b8048f
chapmanb <chapmanb@50mail.com>
2017-09-05 13:51:34 -0400

    Support for bunny from cwlrun

--- scripts/bcbio_vm.py
+++ scripts/bcbio_vm.py
@@ -156,7 +156,7 @@ def _cwl_cmd(subparsers):
 
 def _cwlrun_cmd(subparsers):
     parser = subparsers.add_parser("cwlrun", help="Run common workflow language with a specified tool")
-    parser.add_argument("tool", help="CWL tool to run", choices=["cwltool", "arvados", "toil"])
+    parser.add_argument("tool", help="CWL tool to run", choices=["cwltool", "arvados", "toil", "bunny"])
     parser.add_argument("directory", help="Directory with bcbio generated CWL")
     parser.add_argument("toolargs", help="Arguments to pass to CWL tool", nargs="*")
     parser.add_argument('--no-container', help="Use local installation of bcbio instead of Docker container",
